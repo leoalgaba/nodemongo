b875ce5ed5c79157bcc9c653714746f020b4ef96
leoalgaba <leo_algaba@hotmail.com>
2021-12-16 14:26:53 +0100

    make it

--- src/database.js
+++ src/database.js
@@ -3,9 +3,7 @@ import {MONGODB_URI} from './config'
 
 (async () => {
     try {
-        const db = await connect(
-          "mongodb+srv://admin:admin@cluster0.oastl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-        );
+        const db = await connect(MONGODB_URI)
         console.log("BD conectada a", db.connection.name);
     } catch (err) {
         console.log(err);
