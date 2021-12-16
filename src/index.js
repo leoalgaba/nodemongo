f650507417d3df82ee462a8967b8ec39b9d7c157
leoalgaba <leo_algaba@hotmail.com>
2021-12-16 15:05:48 +0100

    make it bettear

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app';
 import './database'
 import {PORT} from './config'
 
-app.listen(app.get(PORT));
+app.listen(PORT);
 console.log(`server on port ${PORT}`);
 //app.listen(app.get("port"));
 //console.log(`server on port ${app.get("port")}`);
